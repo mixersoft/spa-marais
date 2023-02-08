e2971ca2907c47e47ed52121c166658191d66060
michael lin <mlininkl@gmail.com>
2023-02-09 00:44:37 +0100

    (feat) config to import @ml/shared

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,8 +1,16 @@
+const { merge } = require("webpack-merge");
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
   // Feel free to modify this webpack config however you'd like to
-  return singleSpaWebpackConfig;
+
+  // "externals" is required for `import { transcriptId } from '@ml/shared';`
+  return merge( singleSpaWebpackConfig, 
+    {
+      externals: [/^@ml\/.+/]
+    }
+  );
+
 };

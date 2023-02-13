02d9684971c23bc4b591eca73ae18853ea2a038b
michael lin <mlininkl@gmail.com>
2023-02-13 14:49:07 +0100

    (feat) add halEditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, AfterViewInit } from '@angular/core';
 // @ts-ignore
-import { InteractivePlayer } from '@ml/shared';
+import { InteractivePlayer, HalEditor, doEdit} from '@ml/shared';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,11 @@ import { InteractivePlayer } from '@ml/shared';
 export class AppComponent implements AfterViewInit {
   title:string = 'Episode 84: Paris - Visite du Marais';
   interactivePlayer:InteractivePlayer;
+  halEdit: HalEditor;
 
   constructor(public element: ElementRef){
     // let rootEl = this.element.nativeElement;
-    this.interactivePlayer = new InteractivePlayer({mediaProvider: "duo-fr"}); 
+    this.interactivePlayer = new InteractivePlayer({mediaProvider: "duo-fr"});
   }
 
   async ngAfterViewInit(){
@@ -27,13 +28,25 @@ export class AppComponent implements AfterViewInit {
 
     try {
       // load audio stream
-      let src = await this.interactivePlayer.loadMedia()
-      this.interactivePlayer.playMediaOnClick()
+      let src = await this.interactivePlayer.loadMedia();
       // const id = "single-spa-application:@ml/marais";
       const switch_lang = container?.querySelector("#set_lang");
       switch_lang?.addEventListener("click", (ev:MouseEvent)=>{
         this.interactivePlayer.toggleLangVisibility(ev, 'hide-en');
       })
+
+      // play or edit, but we're not using angular/router
+      if (doEdit()) {
+        this.halEdit = new HalEditor(this.interactivePlayer.config);
+        this.halEdit.toggleEdit(true, (isEditing:boolean)=>{
+          // ???: does this still work? 
+          // now we are replaying clips directly. when do we call toggleEdit()?
+          this.interactivePlayer.playMediaOnClick(isEditing);
+        });
+      }
+      else {
+        this.interactivePlayer.playMediaOnClick();
+      }
     } catch (err) {
       console.error(err)
     }
